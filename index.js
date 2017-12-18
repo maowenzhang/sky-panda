35b24ce197d29decf59affc47c338c44dbd38ae0
Lori-Maowen Zhang <maowen.zhang@autodesk.com>
2017-12-18 23:56:00 +0800

    handle auto call back

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/oauth/callback', function(req, res) {
+
+	  res.render('pages/index');
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
